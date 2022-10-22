3440860009bae820841b0713d5b920ede71f9cb3
desktopworm85 <39500730+desktopworm85@users.noreply.github.com>
2022-10-22 14:57:32 +0100

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,11 +7,9 @@ public class Main
 	public static void main(String[] args)
 	{
 		JFrame window = new JFrame();
-		//JLabel score = new JLabel("test");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setName("Pong");
-		//score.setBounds(50,50,160,20);
 		
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
